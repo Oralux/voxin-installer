d481d9d85f17e132ce263945b76834642fdd2a67
Gilles Casse <gcasse@oralux.org>
2018-12-10 23:02:34 +0100

    Get the old libstc++ archive

--- src/libstdc++.sh
+++ src/libstdc++.sh
@@ -6,34 +6,6 @@ BASE=$(realpath $(dirname "$0")/..)
 VERSION=1.0-1
 LIBSTDCPP_ORIG_NAME=libstdc++2.10-glibc2.2
 NAME=voxin-libstdc++
-ARCH=all
-
-getControl() {
-	local name=$1
-	local version=$2
-	local size=$3
-	local description=$4
-	local control=$5
-	local arch=$6
-cat <<EOF>$control
-Package: $name
-Version: $version
-Architecture: all
-Maintainer: Gilles Casse <gcasse@oralux.org>
-Section: sound
-Priority: optional
-Installed-Size: $size
-Description: $description
- The GNU stdc++ library
- NOTE: This is not a final release, but taken from the CVS gcc-2_95-branch
- (dated 2001-10-02).
- .
- This package contains an additional runtime library for C++ programs
- built with the GNU compiler.
-
-EOF
-
-}
 
 getChangelog() {
 	local name=$1
@@ -45,6 +17,13 @@ $name ($version) UNRELEASED; urgency=medium
 
   * Installs in the rfs32 directory the original Debian package available at :
     $LIBSTDCPP_URL
+    Described as:
+    "The GNU stdc++ library
+    NOTE: This is not a final release, but taken from the CVS gcc-2_95-branch
+    (dated 2001-10-02).
+ 
+    This package contains an additional runtime library for C++ programs
+    built with the GNU compiler."
 
  -- Gilles Casse <gcasse@oralux.org>  $date
 
@@ -67,13 +46,9 @@ dpkg-deb -x ../$(basename $LIBSTDCPP_URL) .
 rm -rf DEBIAN/*
 mv usr $rfs32/
 
-doc=$dst/usr/share/doc/$NAME
+doc=$(dirname $rfs32)/share/doc/$NAME
 mkdir -p "$doc"
 getChangelog $NAME $VERSION $doc/changelog.Debian
 gzip -9n $doc/changelog.Debian
 mv $rfs32/usr/share/doc/$LIBSTDCPP_ORIG_NAME/copyright $doc
-getOverride $NAME $dst "package-contains-timestamped-gzip arch-independent-package-contains-binary-or-object unstripped-binary-or-object missing-depends-line new-package-should-close-itp-bug old-fsf-address-in-copyright-file shlib-with-executable-stack"
-buildpkg $NAME $VERSION "installs $LIBSTDCPP_ORIG_NAME to the 32 bits root filesystem" $dst
 
-
-lintian $PKGDIR/${NAME}_${VERSION}_all.deb
