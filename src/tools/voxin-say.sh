d9dbf29f12bf53d48247b1f83fb3169ec441140e
Gilles Casse <gcasse@oralux.org>
2019-01-05 00:04:43 +0100

    debug tools/voxin-say.sh: local install

--- src/tools/voxin-say.sh
+++ src/tools/voxin-say.sh
@@ -2,12 +2,11 @@
 
 BASE=$(realpath "$(dirname "$0")/..")
 
-
 ## TODO (conf)
 
 # look for the current installation path (local or system-wide)
-LOCAL1=$BASE/.oralux/rfs
-LOCAL2=$HOME/.oralux/rfs
+LOCAL1=$BASE/rfs
+LOCAL2=$BASE/.oralux/rfs
 GLOBAL=/
 
 VOXINDIR=/opt/oralux/voxin
@@ -18,10 +17,19 @@ done
 
 ##
 
+[ -z "$RFSDIR" ] && echo "voxin directory not found!" && exit 1
+
+unset PLAY
+for i in aplay paplay; do
+	a=$(which $i) && PLAY=$i && break
+done
+
+[ -z "$PLAY" ] && echo "install aplay or paplay" && exit 1
+
 ./setConf.sh "$RFSDIR"
 
 export LD_LIBRARY_PATH="$RFSDIR/$VOXINDIR/lib"
-"$VOXINDIR"/bin/voxin-say $@
+"$RFSDIR/$VOXINDIR"/bin/voxin-say $@ | $PLAY
 
 #"$VOXINDIR"/bin/voxin-say."$(uname -m)" $@
 
