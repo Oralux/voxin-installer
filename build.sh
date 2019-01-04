0eaac5ac29aec9bc27b6c961384bb52f75aa6c52
Gilles Casse <gcasse@oralux.org>
2019-01-04 12:04:29 +0100

    Build buildroot, libvoxin, download libstdc++. Smaller script.

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash -xv
 
 BASE=$(dirname $(realpath "$0"))
 NAME=$(basename "$0")
-
-
-echo "Work in progress"
-exit 1
+. $BASE/src/conf.inc
 
 usage() {
 	echo "
@@ -18,28 +15,23 @@ Note that a minimal 32 bits root filesystem is needed as input to this
 script (see README.org).
 
 Options: 
--c	clean-up: delete the build directory and object files
+-c	clean-up: delete the build directory and object files (buildroot, tarballs,...)
 -h  display this help 
--r  (optionally) build voxin-rfs32
-    The standard 32 bits libraries should have been built by Buildroot (https://buildroot.org). 
+-b  (optionally) build a 32 bits rootfilesystem using buildroot (https://buildroot.org). 
     see its configuration in src/buildroot/
--l  (optionally) build voxin-libstdc++
-    voxin-libstdc++ installs the libstdc++ library in the /opt/voxin/rfs32 directory.
+
 
 Example:
-# build the libvoxin and voxind packages
+# build all
  $0
-
-# build all packages
- $0 -clr
 
 " 
 
 }
 
-unset CLEAN HELP CPP RFS32 
+unset CLEAN HELP BUILDROOT 
 
-OPTIONS=`getopt -o chlr \
+OPTIONS=`getopt -o chb \
              -n "$NAME" -- "$@"`
 [ $? != 0 ] && usage && exit 1
 eval set -- "$OPTIONS"
@@ -48,8 +40,7 @@ while true; do
   case "$1" in
     -c) CLEAN=1; shift;;
     -h) HELP=1; shift;;
-    -l) CPP=1; shift;;
-    -r) RFS32=1; shift;;
+    -b) BUILDROOT=1; shift;;
     --) shift; break;;
     *) break;;
   esac
@@ -57,14 +48,17 @@ done
 
 [ -n "$HELP" ] && usage && exit 0
 
-if [ -n "$CLEAN" ]; then
-	rm -rf $BASE/build
-	find $BASE -name "*~" ! -path "*.git*" -exec rm {} \;
-fi
+## TODO
+# if [ -n "$CLEAN" ]; then
+# 	rm -rf $BASE/build
+# 	find $BASE -name "*~" ! -path "*.git*" -exec rm {} \;
+# fi
 
-[ -n "$RFS32" ] && $BASE/src/rfs32.sh
-[ -n "$CPP" ] && $BASE/src/libstdc++.sh
 
-# build libvoxin
-$BASE/src/libvoxin.sh
+mkdir -p "PKGDIR" "DWLDIR" "RFS32"
+[ -n "$BUILDROOT" ] && buildBuildroot
+getMinimalRFS32FromBuildroot
+getOldLibstdc++
+getLibvoxin
 
+echo "rfs: $RFS_DIR"
