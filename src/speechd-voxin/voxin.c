a9bbbfb1d7706584601bb33d7d7cf856323ed57c
Gilles Casse <gcasse@oralux.org>
2019-03-11 00:13:18 +0100

    (Cont.) Compatibility with multiple speech-dispatcher versions

---
+++ src/speechd-voxin/voxin.c
@@ -0,0 +1,51 @@
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_LINE 30
+extern char **environ;
+
+int main(int argc, char *argv[])
+{
+  char line[MAX_LINE];
+  char *version = NULL;
+  char *module = NULL;
+  static char path[PATH_MAX+10];  
+  FILE *fd = popen("/usr/bin/speech-dispatcher -v", "r");
+  if (fd == NULL) {
+    perror("sd_voxin");
+    return 1;
+  }
+  if (!fgets(line, MAX_LINE, fd)) {
+    return 2;
+  }
+
+  pclose(fd);  	
+
+  if (!realpath(*argv, path)) {
+	return 4;
+  }
+
+  char *basename = strrchr(path, '/');
+  *basename = 0;
+  chdir(path);
+
+  version = strrchr(line, ' ');
+  if (!version || (strlen(version) < 4))
+	return 3;
+  version++;
+  version[strlen(version)-1] = 0;
+  if (!strncmp(version, "0.9", 3)) {
+	module = SD9;
+  } else if (!strncmp(version, "0.8", 3)) {
+	module = SD8;
+  } else if (!strncmp(version, "0.7", 3)) {
+	module = SD7;
+  }
+
+  execve(module, argv, environ);
+  
+  return 0;
+}
