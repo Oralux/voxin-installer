02f5d76bfe21ae2b70070cb6f285041eb8796d90
Gilles Casse <gcasse@oralux.org>
2025-11-01 17:20:09 +0100

    rsync.sh: optional argument for the SSH port of the build server
    usage ./rsync.sh <arch> [<port>]

--- rsync.sh
+++ rsync.sh
@@ -5,35 +5,38 @@ BASE=$(dirname $(realpath "$0"))
 . $BASE/src/host.inc
 . $BASE/src/conf.inc
 
-if [ $# != 1 ]; then
-    echo "usage ./rsync.sh <arch>"
+if [ $# = 0 ] || [ $# -gt 2 ]; then
+    echo "usage ./rsync.sh <arch> [<port>]"
     echo "<arch>: x86_64, armv7l or aarch64"
+    echo "<port>: ssh port (by default 22)"
     exit 1
 fi
 
 ARCH=$1
+SSH_PORT=${2:-22}
+
 getBuildServer "$ARCH"
 SERVER=$BUILD_USER@$BUILD_HOST
 EXTRA_DIR=voxin-viavoice
 
 upload() {
 	pushd $HOME/VOXIN
-	rsync --delete -aRvz voxin-installer $SERVER:$VMVOXDIR/
-	rsync --delete -avR voxin-viavoice/build/packages $SERVER:$VMVOXDIR/
+	rsync --delete -aRvz -e "ssh -p $SSH_PORT" voxin-installer $SERVER:$VMVOXDIR/
+	rsync --delete -avR voxin-viavoice/build/packages -e "ssh -p $SSH_PORT" $SERVER:$VMVOXDIR/
 	popd
 }
 
 download() {
 	pushd $HOME/VOXIN
-	rsync  $1 -av $SERVER:$VMVOXDIR/voxin-installer/build/packages/ voxin-installer/sav.voxin_$VOXIN_VERSION
-	rsync  $1 -av $SERVER:$VMVOXDIR/voxin-installer/check/$VOXIN_VERSION voxin-installer/check/
+	rsync  $1 -av -e "ssh -p $SSH_PORT" $SERVER:$VMVOXDIR/voxin-installer/build/packages/ voxin-installer/sav.voxin_$VOXIN_VERSION
+	rsync  $1 -av -e "ssh -p $SSH_PORT" $SERVER:$VMVOXDIR/voxin-installer/check/$VOXIN_VERSION voxin-installer/check/
 	popd
 }
 
 build() {
-    [ "$ARCH" = "x86_64" ] && ssh $SERVER "set -e; cd $VMVOXDIR/voxin-installer && ./build.sh -t src/list.vv"
-    ssh $SERVER "set -e; cd $VMVOXDIR/voxin-installer && ./build.sh -t src/list.ve.$ARCH"
-    rsync -av $SERVER:$VMVOXDIR/voxin-installer/build/tmp/voxin-installer_$VOXIN_VERSION/packages/all/ sd_voxin_tarballs/
+    [ "$ARCH" = "x86_64" ] && ssh -p $SSH_PORT $SERVER "set -e; cd $VMVOXDIR/voxin-installer && ./build.sh -d -t src/list.vv"
+    ssh -p $SSH_PORT $SERVER "set -e; cd $VMVOXDIR/voxin-installer && ./build.sh -d -t src/list.ve.$ARCH"
+    rsync -av -e "ssh -p $SSH_PORT" $SERVER:$VMVOXDIR/voxin-installer/build/tmp/voxin-installer_$VOXIN_VERSION/packages/all/ sd_voxin_tarballs/
 }
 
 echo "Memo: voxind-nve rsynced? (y|N)"
